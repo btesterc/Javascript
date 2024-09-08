83d3031d3c228500abb13dba2b00648c4e3201a7
burak celik <brkclk.ch@gmail.com>
2024-09-08 13:25:02 +0200

    primitive variables

--- experiment.js
+++ experiment.js
@@ -1,19 +1,37 @@
-const monthlyRent = 500;
+// const monthlyRent = 500;
 
-let yearlyRent = monthlyRent * 12;
+// let yearlyRent = monthlyRent * 12;
 
-console.log(yearlyRent, monthlyRent);
-
-
-yearlyRent = 1;
-
-console.log(yearlyRent);
+// console.log(yearlyRent, monthlyRent);
 
 
-const firstName = "Brian";
-const lastName = "Holt";
+// yearlyRent = 1;
 
-const sentence = `Hello ${firstName} ${lastName}! How are you !?`;
+// console.log(yearlyRent);
 
-console.log(sentence);
 
+// const firstName = "Brian";
+// const lastName = "Holt";
+
+// const sentence = `Hello ${firstName} ${lastName}! How are you !?`;
+
+// console.log(sentence);
+
+
+// const isTheSkyBlue = true;
+
+// console.log(isTheSkyBlue);
+
+// let myName = "Brian";
+// myName= 15;
+
+// myName = true;
+
+// console.log(myName);
+
+// const num = 5.0000001;
+
+// console.log(num + 1.123 / 2);
+
+
+const isSkyBlue = true;
