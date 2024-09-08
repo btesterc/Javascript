cc5de5fa72b9582a3edd83c6a23d021ab412dfe6
burak celik <brkclk.ch@gmail.com>
2024-09-08 13:03:04 +0200

    first commit

---
+++ experiment.js
@@ -0,0 +1,19 @@
+const monthlyRent = 500;
+
+let yearlyRent = monthlyRent * 12;
+
+console.log(yearlyRent, monthlyRent);
+
+
+yearlyRent = 1;
+
+console.log(yearlyRent);
+
+
+const firstName = "Brian";
+const lastName = "Holt";
+
+const sentence = `Hello ${firstName} ${lastName}! How are you !?`;
+
+console.log(sentence);
+
