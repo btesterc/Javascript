c9ad9056e703ae5b2575c2be51e9ce706b3f3c39
burak celik <brkclk.ch@gmail.com>
2024-09-09 13:54:22 +0200

    objects

--- experiment.js
+++ experiment.js
@@ -181,4 +181,103 @@
 // console.log(F); // works, F was declared in the global scope
 
 
-// ------------- Built in Functions ---------------
+// -------------- Objects ---------------- 
+
+const person = {
+    name: "Burak Celik",
+    city: "Seattle",
+    state: "WA",
+    favoriteFood: "🌮",
+    wantsTacosRightNow: true,
+    numberOfTacosWanted: 100
+  };
+  console.log(person);
+  console.log(person.name);
+  console.log(person["name"]); // same as the line above; definitely prefer using the other one
+
+// ----------------------------------------------------
+
+  const person1 = {
+    name: "burak",
+    ageRange: "25-35"
+  };
+  const person2 = {
+    name: "seda",
+    ageRange: "65-75"
+  };
+  
+  function suggestMusic(person) {
+    if (person.ageRange === "25-35") {
+      console.log("We think you'll like Daft Punk you crazy millenial.");
+    } else if (person.ageRange === "65-75") {
+      console.log(
+        "You're obviously going to like Johnny Cash. He walks the line."
+      );
+    } else {
+      console.log(
+        "Uh, maybe try David Bowie? Everyone likes David Bowie, right?"
+      );
+    }
+  }
+  
+  suggestMusic(person1);
+  suggestMusic(person2);
+
+//   --------------------------------------------------
+
+  const dog = {
+    name: "dog",
+    speak() {
+      console.log("woof woof");
+    }
+  };
+  
+  dog.speak();
+
+//   ----------------------------------------------------
+
+const mee = {
+    name: {
+      first: "Brian",
+      last: "Holt"
+    },
+    location: {
+      city: "Seattle",
+      state: "WA",
+      country: "USA"
+    }
+  };
+  
+  console.log(mee);
+
+
+//   ---------------------------------------------------
+
+const me = {
+    name: {
+      first: "Brian",
+      last: "Holt"
+    },
+    location: {
+      streetNumber: 500,
+      street: "Fakestreet",
+      city: "Seattle",
+      state: "WA",
+      zipCode: 55555,
+      country: "USA"
+    },
+    getAddress() {
+      return `${this.name.first} ${this.name.last}
+  ${this.location.streetNumber} ${this.location.street}
+  ${this.location.city}, ${this.location.state} ${this.location.zipCode}
+  ${this.location.country}`;
+    }
+  };
+  
+  console.log(me.getAddress());
+
+  me.name.first = "niki"
+
+  console.log(me.name)
+
+//   ------------------- Arrays ---------------------------
