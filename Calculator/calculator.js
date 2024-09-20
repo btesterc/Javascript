29adac51fd7865271f2a1da8f55f289b16e50e47
burak celik <brkclk.ch@gmail.com>
2024-09-20 16:18:02 +0200

    numberstrings

---
+++ Calculator/calculator.js
@@ -0,0 +1,34 @@
+let runningTotal = 0;
+let buffer = "0";
+let priviousOperator;
+
+const screen = document.querySelector(".screen");
+
+function buttonClick(value) {
+    if (isNaN(value)) {
+        // this is not a number
+        handleSymbol(value);
+    } else {
+        // this is a number
+        handleNumber(value);
+    }
+}
+
+function handleSymbol(symbol) {}
+
+function handleNumber(numberString) {
+        if (buffer === "0") {
+            buffer = numberString;
+        } else {
+            buffer += numberString;
+        }
+        screen.innerText = buffer;
+    }
+
+function init () {
+    document.querySelector(".calc-buttons").addEventListener('click', function(event) {
+        buttonClick(event.target.innerText);
+    })
+}
+
+init ();
