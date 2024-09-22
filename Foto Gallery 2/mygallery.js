160e5f4d5b2683db7997c9cb8a59da4fc0681844
burak celik <brkclk.ch@gmail.com>
2024-09-22 22:27:15 +0200

    swiper

--- Foto Gallery 2/mygallery.js
+++ Foto Gallery 2/mygallery.js
@@ -1,6 +1,8 @@
 new Swiper(".swiper-container", {
     speed: 400,
     spaceBetween: 100,
+    effect: "fade",
+    loop: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
